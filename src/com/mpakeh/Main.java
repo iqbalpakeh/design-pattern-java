3ede7f11f0fc29a390e89c116ff973d1e031d706
Progrema Studio <iqbalpakeh@gmail.com>
2020-01-23 04:21:18 +0700

    minor update on code

--- src/com/mpakeh/Main.java
+++ src/com/mpakeh/Main.java
@@ -5,6 +5,6 @@ import com.mpakeh.observer.ObserverPatternDemo;
 public class Main {
 
     public static void main(String[] args) {
-        ObserverPatternDemo.demo();
+        ObserverPatternDemo.run();
     }
 }
