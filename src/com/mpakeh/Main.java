128154e984332148ad2a8768849c87db2b8bb350
Progrema Studio <iqbalpakeh@gmail.com>
2020-01-22 19:35:29 +0700

    Add Observer Design Pattern

---
+++ src/com/mpakeh/Main.java
@@ -0,0 +1,10 @@
+package com.mpakeh;
+
+import com.mpakeh.observer.ObserverPatternDemo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ObserverPatternDemo.demo();
+    }
+}
