12b225524f3a42bed497e07e2844b3cc92ead858
Progrema Studio <iqbalpakeh@gmail.com>
2020-01-23 04:52:56 +0700

    add singleton pattern

--- src/com/mpakeh/Main.java
+++ src/com/mpakeh/Main.java
@@ -1,10 +1,12 @@
 package com.mpakeh;
 
 import com.mpakeh.observer.ObserverPatternDemo;
+import com.mpakeh.singletone.SingletonePatternDemo;
 
 public class Main {
 
     public static void main(String[] args) {
         ObserverPatternDemo.run();
+        SingletonePatternDemo.run();
     }
 }
